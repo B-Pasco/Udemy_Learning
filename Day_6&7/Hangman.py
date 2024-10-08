7937d5702b0045555bb84c9ec24a875a63488682
B-Pasco <mugishapasco02@gmail.com>
2024-10-08 23:15:58 +0200

    hangman

--- Day_6&7/Hangman.py
+++ Day_6&7/Hangman.py
@@ -12,16 +12,35 @@ for position in range(word_length):
     placeholder += "_"
 print(placeholder)
 
-Guess = input("Guess a letter: ").lower()
-print(Guess)
+lives = 6
+game_over = False
+correct_letter = []
+
+while not game_over:
+    Guess = input("Guess a letter: ").lower()
+    print(Guess)
 
 
-display = ""
+    display = ""
 
-for letter in chosen_word:
-    if letter == Guess:
-        display += letter
-    else:
-        display += "_"
+    for letter in chosen_word:
+        if letter == Guess:
+            display += letter
+            correct_letter.append(Guess)
+        elif letter in correct_letter:
+            display += letter
+        else:
+            display += "_"
 
-print(display)
+    print(display)
+
+
+    if Guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            print("You lose")
+
+
+    if "_" not in display:
+        game_over = True
+        print("You win!...")
