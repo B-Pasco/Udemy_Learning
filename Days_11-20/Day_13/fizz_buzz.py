534a491a56ddfba4710b27097955fa5627261c24
B-Pasco <mugishapasco02@gmail.com>
2024-11-06 21:27:23 +0200

    debugging fizzBuzz

---
+++ Days_11-20/Day_13/fizz_buzz.py
@@ -0,0 +1,18 @@
+# Target is the number up to which we count
+# The actual target was to debug this code of game.
+def fizz_buzz(target):
+    for number in range(1, target + 1):
+        if number % 3 == 0 and number % 5 == 0:
+            number = "FizzBuzz"
+            print(number)
+        elif number % 3 == 0:
+            number = "Fizz"
+            print(number)
+        elif number % 5 == 0:
+            number = "Buzz"
+            print(number)
+        else:
+            print(number)
+
+
+fizz_buzz(20)
